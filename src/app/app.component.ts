2e41d889c5a8d0fb70b403b5afc8ca8928ea6372
lajosf <lajosf@outlook.com>
2017-10-22 14:22:20 +0200

    Create a new simple module for the product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'pm-root',
   template: `
     <div><h1>{{pageTitle}}</h1>
-      <div>Helo Szabi! Here is my first Angular component :D</div>
+      <pm-products></pm-products>
     </div>
     `
 })
